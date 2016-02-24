db1335400bef2b2f2146491ea89208f4a28d3400
dev080 <mailafpa5@gmail.com>
2016-02-24 16:46:18 +0100

    ajout particularite listbox.

--- Avec-Modif/FilRouge/FilRougeDAO/Form5.cs
+++ Avec-Modif/FilRouge/FilRougeDAO/Form5.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,48 +18,70 @@ namespace FilRougeDAO
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            // met dans label8 la valeur de la réduction du client
-            SqlConnection connect = new SqlConnection("server=(local); integrated security = true; database = FilRouge_essai_modif");
-            connect.Open();
-
-            SqlCommand requete = new SqlCommand("select CoeffClient from client where NomClient='" + listBox1.SelectedItem + "'", connect);
-
-            SqlDataReader resultat = requete.ExecuteReader();
-            while (resultat.Read())
+            if (listBox2.SelectedItem != null)
             {
-                label8.Text = (resultat["CoeffClient"].ToString());
+                // met dans label8 la valeur de la réduction du client
+                SqlConnection connect = new SqlConnection("server=(local); integrated security = true; database = FilRouge_essai_modif");
+                connect.Open();
+
+                if (listBox2.SelectedItem.GetType() == typeof(USState))
+                {
+                    USState d = (USState)listBox2.SelectedItem;
+
+                    MessageBox.Show("valeur " + d.LongName + " valeur2");
+                }
+
+
+
+
+                SqlCommand requete = new SqlCommand("select CoeffClient from client where IdClient=" + listBox1.SelectedValue, connect);
+                SqlDataReader resultat = null;
+                try { resultat = requete.ExecuteReader(); }
+                catch (Exception r)
+                { MessageBox.Show(r.Message); }
+
+                while (resultat.Read())
+                {
+                    label8.Text = (resultat["CoeffClient"].ToString());
+
+                }
+                resultat.Close();
+
+
+                //place l'indice du trackbar
+                requete = new SqlCommand("select Reduction from Client where Idclient=" + listBox1.SelectedValue + "", connect);
+                resultat = requete.ExecuteReader();
+                while (resultat.Read())
+                {
+                    trackBar1.Value = Int32.Parse((resultat["Reduction"].ToString()));
+
+                }
+                resultat.Close();
+
+                connect.Close();
+
 
             }
-            resultat.Close();
-
-
-            //place l'indice du trackbar
-            requete = new SqlCommand("select Reduction from Client where Nomclient='" + listBox1.SelectedItem + "'", connect);
-            resultat = requete.ExecuteReader();
-            while (resultat.Read())
-            {
-                trackBar1.Value = Int32.Parse((resultat["Reduction"].ToString()));
-
-            }
-            resultat.Close();
-
-            connect.Close();
-
-
-
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            MessageBox.Show(listBox1.SelectedValue + " " + listBox1.SelectedItem.ToString());
 
+
+            //SqlConnection connect = new SqlConnection("server=(local); integrated security = true; database = FilRouge_essai_modif");
+            //connect.Open();
+
+            //SqlCommand requete = new SqlCommand("", connect);
+            //requete.Parameters.AddWithValue();
+
+
+            //requete.ExecuteNonQuery();
+
+            //connect.Close();
         }
 
         private void trackBar1_Scroll(object sender, EventArgs e)
@@ -66,7 +89,7 @@ namespace FilRougeDAO
             //enregistre le nouveau montant de la réduction
             SqlConnection connect = new SqlConnection("server=(local); integrated security = true; database = FilRouge_essai_modif");
             connect.Open();
-            SqlCommand requete = new SqlCommand("update Client set reduction=" + trackBar1.Value + " where NomClient='" + listBox1.SelectedItem + "'", connect);
+            SqlCommand requete = new SqlCommand("update Client set reduction=" + trackBar1.Value + " where IdClient='" + listBox1.SelectedValue + "'", connect);
 
             requete.ExecuteNonQuery();
 
@@ -91,31 +114,91 @@ namespace FilRougeDAO
 
             SqlCommand requete = new SqlCommand("select * from client", connect);
             SqlDataReader resultat = requete.ExecuteReader();
+
+            ArrayList USStates = new ArrayList();
+
             while (resultat.Read())
             {
-                listBox1.Items.Add(resultat["NomClient"].ToString());
+                //listBox1.Items.Add();
+                USStates.Add(new USState(resultat["IdClient"].ToString(), resultat["NomClient"].ToString()));
 
             }
+
+
+
+
+
+            listBox1.DataSource = USStates;
+            listBox1.DisplayMember = "LongName";
+            listBox1.ValueMember = "ShortName";
+
             resultat.Close();
 
 
+
+
+
             //affiche la liste déroulante des instruments
-            requete = new SqlCommand("select * from produit", connect);
-            resultat = requete.ExecuteReader();
+            SqlCommand requete2 = new SqlCommand("select * from produit", connect);
+            resultat = requete2.ExecuteReader();
+            ArrayList USStates2 = new ArrayList();
+
             while (resultat.Read())
             {
-                listBox2.Items.Add(resultat["NomProdCourt"].ToString());
+                //listBox1.Items.Add();
+                USStates2.Add(new USState(resultat["IdProduit"].ToString(), resultat["NomProdCourt"].ToString()));
 
             }
+
+
+
+
+            listBox2.DisplayMember = "LongName";
+            listBox2.ValueMember = "ShortName";
+            listBox2.DataSource = USStates2;
+
             resultat.Close();
 
             connect.Close();
 
 
+        }
 
+        public class USState
+        {
+            private string myID;
+            private string mycliName;
 
+            public USState(string strShortName, string strLongName)
+            {
+
+                this.myID = strShortName;
+                this.mycliName = strLongName;
+            }
+
+            public string ShortName
+            {
+                get
+                {
+                    return myID;
+                }
+            }
+
+            public string LongName
+            {
+
+                get
+                {
+                    return mycliName;
+                }
+            }
 
         }
+
+
+
+
+
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
@@ -127,7 +210,7 @@ namespace FilRougeDAO
             //else
             //{
             //    //test du stock
-                
+
             //    SqlConnection connect = new SqlConnection("server=(local); integrated security = true; database = FilRouge_essai_modif");
             //    connect.Open();
             //    SqlCommand requete = new SqlCommand("select StockProduit from produit", connect);
@@ -140,11 +223,7 @@ namespace FilRougeDAO
             //    }
             //    //MessageBox.Show("Test du stock");
 
-            //}
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
+            //}
 
         }
 
@@ -154,6 +233,64 @@ namespace FilRougeDAO
             Form4 z = new Form4();
             z.ShowDialog();
             Show();
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+
+                double total = double.Parse(textBox1.Text) * double.Parse(textBox2.Text);
+
+                double reduction = double.Parse(label8.Text) + trackBar1.Value;
+                MessageBox.Show("la somme des deux pourcentages =" + reduction);
+
+                reduction = reduction / 100;
+
+                MessageBox.Show("la somme des deux pourcentages =" + reduction);
+
+                double temp = (total) * (reduction);
+
+                MessageBox.Show("temp vaut " + temp);
+
+                label6.Text = (total - temp).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Choisissez une quantité");
+
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //    SqlConnection connect = new SqlConnection("server=(local); integrated security = true; database = FilRouge_essai_modif");
+            //    connect.Open();
+            //    SqlCommand requete = new SqlCommand("select PrixHT_Prod from produit where IdProduit ='" + listBox2.SelectedValue + "'", connect);
+            //    SqlDataReader resultat = requete.ExecuteReader();
+            //    if (resultat.Read())
+            //    {
+            //        textBox2.Text = resultat["prixHT_Prod"].ToString();
+
+
+            //    }
+            //    resultat.Close();
+
+            //    connect.Close();
+            try
+            {
+                USState test = (USState)listBox2.SelectedItem;
+
+
+                Console.WriteLine(test.LongName);
+                Console.WriteLine(test.ShortName);
+
+            }
+            catch (Exception er)
+            {
+
+            }
         }
     }
 }
--- Avec-Modif/FilRouge/FilRougeDAO/Program.cs
+++ Avec-Modif/FilRouge/FilRougeDAO/Program.cs
@@ -18,7 +18,7 @@ namespace FilRougeDAO
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form5());
 
         }
     }
